04af52911b930b9f103c273b827199f80586cdee
sheng chen <shengj0935@gmail.com>
2025-10-02 08:09:03 +0800

    store and carts functionality

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'carts',
     'store',
     'accounts',
     'category',
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carts.context_processor.counter',
             ],
         },
     },
--- greatkart/store/admin.py
+++ greatkart/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Variation
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -7,5 +7,12 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_name','price','stock','category','is_available','modified_date','images')
     list_editable = ('price','stock','is_available')
     list_per_page = 20
-    
+ 
+class VariationAdmin(admin.ModelAdmin):
+    list_display = ('product','variation_category','variation_value','is_active','created_date')
+    list_editable = ('is_active',)
+    list_filter = ('product','variation_category','variation_value')
+    # list_per_page = 20
+        
 admin.site.register(Product,ProductAdmin)
+admin.site.register(Variation,VariationAdmin)
