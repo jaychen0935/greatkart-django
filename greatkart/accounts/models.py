6e7ccef4c1aac378a1c69fa1510722c9963e6363
sheng chen <shengj0935@gmail.com>
2025-10-05 01:47:51 +0800

    store and carts functionality

--- greatkart/accounts/models.py
+++ greatkart/accounts/models.py
@@ -13,6 +13,7 @@ class MyAccountManager(BaseUserManager):
             username = username,
             first_name = first_name,
             last_name = last_name,
+            
         )
         user.set_password(password)
         user.save(using=self._db)
